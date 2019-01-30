7d4730cdf60bcf24658b5b0b381092774007a16a
wenhai <1194630180@qq.com>
2019-01-30 17:34:14 +0800

    添加控制器内容

--- app/controllers/authorController.js
+++ app/controllers/authorController.js
@@ -1,3 +1,46 @@
 /**
  * Created by Administrator on 2019/1/30 0030.
  */
+
+const Author=require("../models/author");
+
+exports.author_list=function (req,res) {
+    res.send('NOT IMPLEMENTED: Author list');
+}
+
+exports.author_detail=function (req, res) {
+    res.send('NOT IMPLEMENTED: Author detail'+req.params+id);
+};
+
+exports.author_create_get=function (req,res) {
+    res.send('NOT IMPLEMENTED: Author create Get');
+};
+
+
+exports.author_create_post=function(req,res){
+    res.send('NOT IMPLEMENTED: Author create Post');
+};
+
+exports.author_delete_get=function(req,res){
+    res.send('NOT IMPLEMENTED: Author delete Get');
+};
+
+exports.author_delete_post=function(req,res){
+  res.send('NOT IMPLEMENTED: Author delete POST');
+};
+
+exports.author_update_get=function(req,res){
+    res.send('NOT IMPLEMENTED: Author update Get');
+};
+
+exports.author_update_post=function(req,res){
+    res.send('NOT IMPLEMENTED: Author update POST');
+};
+
+
+
+
+
+
+
+
