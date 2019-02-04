a050a44a9cc3fc62ae777a4c1dccf75e00bbad0b
wenhai <1194630180@qq.com>
2019-02-04 16:24:55 +0800

    添加控制器内容

--- app/controllers/authorController.js
+++ app/controllers/authorController.js
@@ -1,43 +1,28 @@
-/**
- * Created by Administrator on 2019/1/30 0030.
- */
+const Author =require('../models/author');
 
-const Author=require("../models/author");
+//显示作者列表
+exports.author_list=(req,res)=>{res.send("为实现：作者列表")};
 
-exports.author_list=function (req,res) {
-    res.send('NOT IMPLEMENTED: Author list');
-}
+//为每位作者显示详细信息页面
+exports.author_detail=(req,res)=>{res.send("为实现：作者详细信息"+req.params.id)};
 
-exports.author_detail=function (req, res) {
-    res.send('NOT IMPLEMENTED: Author detail'+req.params+id);
-};
+// 由GET 显示创建作者的表单
+exports.author_create_get=(req,res)=>{res.send("未实现：作者创建表单的GET")};
 
-exports.author_create_get=function (req,res) {
-    res.send('NOT IMPLEMENTED: Author create Get');
-};
+// 由 POST 处理作者创建操作
+exports.author_create_post=(req,res)=>{res.send("未实现：创操作者POST")};
 
+// 由GET 显示删除作者的表单
+exports.author_delete_get=(req,res)=>{ers.send("未实现 ：删除作者表单GET")};
 
-exports.author_create_post=function(req,res){
-    res.send('NOT IMPLEMENTED: Author create Post');
-};
+// 由 POST 处理作者删除操作
+exports.author_delete_post=(req,res)=>{res.send("未实现 ：删除作者操作POST")};
 
-exports.author_delete_get=function(req,res){
-    res.send('NOT IMPLEMENTED: Author delete Get');
-};
+// 由GET 显示更新作者的表单
+exports.author_update_get=(req,res)=>{res.send("未实现： 更表单页面GET")};
 
-exports.author_delete_post=function(req,res){
-  res.send('NOT IMPLEMENTED: Author delete POST');
-};
-
-exports.author_update_get=function(req,res){
-    res.send('NOT IMPLEMENTED: Author update Get');
-};
-
-exports.author_update_post=function(req,res){
-    res.send('NOT IMPLEMENTED: Author update POST');
-};
-
-
+// 由 POST 处理作者更新操作
+exports.author_uptate_post=(req,res)=>{res.send("未实现： 处理作者更新操作POST")};
 
 
 
